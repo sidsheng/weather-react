f6a9ddf2cb62b5a2fd364b949d09c7fd22196d63
sidsheng <sidsheng@gmail.com>
2020-07-31 09:19:23 +1000

    finished

---
+++ src/components/Main.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import Context from "../Context"
+import Header from "./Layout/Header"
+import Content from "./Layout/Content"
+import Tagline from "./Tagline"
+import WeatherSearch from "./WeatherSearch"
+import WeatherData from "./WeatherData"
+import Error from "./Error"
+
+const API_KEY = "659b6b32c0c2615b7c81b67bc41e5c1c"
+
+export default function Main() {
+    const [weather, setWeather] = useState(null)
+    const [city, setCity] = useState(null) 
+    const [error, setError] = useState(null)
+
+    const api_call = async e => {
+        e.preventDefault()
+        const location = e.target.elements.city.value
+        if (!location) {
+          return setError("Please enter the name of the city"), setWeather(null)
+        }
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`
+        const request = axios.get(url)
+        const response = await request
+        setError(null)
+        setWeather(response.data.main)
+        setCity(response.data.name)
+      }
+
+    return (
+        <div className="main">
+            <Header />
+            <Content>
+                <Context.Provider value={{api_call, weather, city}}>
+                <Tagline/>
+                <WeatherSearch/>
+                { error !== null && <p>{<Error error={error}/>}</p> }
+                { weather !== null && <WeatherData/> }
+                </Context.Provider>
+            </Content>
+        </div>
+    )
+}
---
+++ src/components/Tagline.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Tagline = () => (
+  <div className="tagline">
+    <p>City name</p>
+  </div>
+)
+
+export default Tagline
